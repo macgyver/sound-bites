8ab328cb9b3414d264e04728c49ec3ad5c45ae18
jz <macgyver@users.noreply.github.com>
2024-07-25 11:49:48 -0400

    rendering clues,answers,and handling input

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,86 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive, computed, watch, onMounted } from 'vue'
+import SoundBite from './components/SoundBite.vue'
+
+const CLUE_INDEX = 0
+const ANSWER_INDEX = 1
+const SPLICE_INDEX = 2
+
+let clues = [
+	['folksy medicine accompaniment', 'sugar', [0, 2]],
+	['discreet', 'careful', [5, 6]],
+	['teach', 'school', [1, 3]],
+]
+
+let guesses = reactive(
+	clues.map(([, answer]) => new Array(answer.length).fill(' ')),
+)
+
+let selectedGuess = computed(() => guesses[selectedWordIndex.value])
+
+let clueEls = ref([])
+
+let selectedWordIndex = ref(0)
+let selectedLetterIndex = ref(0)
+
+watch(selectedWordIndex, (index) => {
+	if (index < 0) selectedWordIndex.value = clues.length - 1
+	if (index >= clues.length) selectedWordIndex.value = 0
+	// todo: move selected letter index to the first blank space in the selected word
+	selectedLetterIndex.value =
+		selectedGuess.value.findIndex((letter) => letter === ' ') || 0
+})
+let selectedWord = computed(() => clues[selectedWordIndex.value][ANSWER_INDEX])
+
+watch(selectedLetterIndex, (index) => {
+	if (index < 0) selectedLetterIndex.value = selectedWord.value.length - 1
+	if (index >= selectedWord.value.length) selectedLetterIndex.value = 0
+})
+onMounted(() => console.log(clueEls.value))
+
+function handleDelete() {
+	console.debug('handling delete')
+	guesses[selectedWordIndex.value].splice(selectedLetterIndex.value, 1, ' ')
+	selectedLetterIndex.value -= 1
+}
+
+function handleInput(e) {
+	if (e.keyCode < 65 || !/\p{L}/u.test(e.key)) return
+	console.debug('*input', e)
+	guesses[selectedWordIndex.value].splice(selectedLetterIndex.value, 1, e.key)
+	selectedLetterIndex.value += 1
+}
 </script>
 
 <template>
 	<header>
-		<img
-			alt="Vue logo"
-			class="logo"
-			src="./assets/logo.svg"
-			width="125"
-			height="125"
-		/>
-
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-		</div>
+		<h1>Sound Bites</h1>
 	</header>
 
-	<main>
-		<TheWelcome />
+	<main
+		autofocus
+		tabindex="0"
+		@keyup.up="selectedWordIndex -= 1"
+		@keyup.down="selectedWordIndex += 1"
+		@keyup.enter="selectedWordIndex += 1"
+		@keyup.left="selectedLetterIndex -= 1"
+		@keyup.right="selectedLetterIndex += 1"
+		@keyup.delete="handleDelete"
+		@keyup="handleInput"
+	>
+		<SoundBite
+			v-for="([clue, answer, bites], index) in clues"
+			ref="clueEls"
+			:key="index"
+			:clue="clue"
+			:answer="answer"
+			:bites="bites"
+			:guess="guesses[index]"
+			:selected="selectedWordIndex === index"
+			:selected-letter-index="selectedLetterIndex"
+			@select="selectedWordIndex = index"
+		/>
 	</main>
 </template>
 
-<style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-}
-</style>
+<style scoped></style>
---
+++ src/components/SoundBite.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { defineProps, defineEmits, ref, reactive, watch } from 'vue'
+
+let props = defineProps({
+	clue: {
+		type: String,
+		required: true,
+	},
+	answer: {
+		type: String,
+		required: true,
+	},
+	bites: {
+		type: Array,
+		required: true,
+	},
+	selected: {
+		type: Boolean,
+		required: true,
+	},
+	selectedLetterIndex: {
+		type: Number,
+		default: 0,
+	},
+	guess: {
+		type: Array,
+		required: true,
+	},
+})
+
+let emit = defineEmits(['select-word', 'select-letter'])
+</script>
+
+<template>
+	<div
+		class="sound-bite"
+		:class="{ selected }"
+		@click="emit('select-word')"
+		tabindex="0"
+		@focus="emit('select-word')"
+	>
+		<span class="clue">{{ clue }}</span>
+		<span class="answer">
+			<span
+				class="letter"
+				v-for="(letter, index) in answer"
+				:key="index"
+				:class="{
+					bite: index >= bites[0] && index < bites[1],
+					selected: selected && selectedLetterIndex === index,
+				}"
+				@click="emit('select-letter', index)"
+				>{{ guess[index] }}</span
+			>
+		</span>
+	</div>
+</template>
+
+<style scoped>
+.sound-bite {
+	display: flex;
+	--girth: 2px;
+	font-size: 2rem;
+}
+
+.answer {
+	display: flex;
+}
+
+.letter {
+	.selected & {
+		background-color: cyan;
+	}
+
+	&.selected {
+		background-color: fuchsia;
+	}
+
+	height: 3rem;
+	aspect-ratio: 1;
+	/* padding: 1rem; */
+	border: var(--girth) solid black;
+	position: relative;
+	text-transform: uppercase;
+	line-height: 1;
+	align-content: center;
+	padding: 0px 0.4em;
+	font-size: 2rem;
+}
+
+.bite {
+	&::after {
+		content: '';
+		position: absolute;
+		top: calc(-0.9 * var(--girth));
+		bottom: calc(-0.9 * var(--girth));
+		right: calc(-0.9 * var(--girth));
+		left: calc(-0.9 * var(--girth));
+		border: var(--girth) solid black;
+		border-radius: 50%;
+	}
+}
+</style>
